1f179b57e61f6463d46fec8aef85f17e6ae95394
Drkainat8 <161299000+Drkainat8@users.noreply.github.com>
2024-05-15 23:39:06 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+//assignment 6 write name with white space and without white space by inserting "\t" for white space "\n" for next line
+var fullname = "\n\tKainat iqbal\t\n";
+console.log(fullname);
---
+++ main.ts
@@ -0,0 +1,3 @@
+//assignment 6 write name with white space and without white space by inserting "\t" for white space "\n" for next line
+let fullname : string = "\n\tKainat iqbal\t\n";
+console.log(fullname)
